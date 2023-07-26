f1fb586ca59ea79bd159d4a46cfae2d7369ff8d2
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2023-07-26 09:00:42 +0530

    Add doc comments

--- src/main/java/io/github/dineshsolanki/timezoneinterceptor/TimeZoneInterceptor.java
+++ src/main/java/io/github/dineshsolanki/timezoneinterceptor/TimeZoneInterceptor.java
@@ -9,13 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.TimeZone;
 
+/**
+ * A class that intercepts requests and sets the time zone for the current thread.
+ */
 public class TimeZoneInterceptor  implements HandlerInterceptor {
     private static final ThreadLocal<TimeZone> TIME_ZONE_THREAD_LOCAL = new ThreadLocal<>();
     private final static Logger logger = LoggerFactory.getLogger(TimeZoneInterceptor.class);
     @Value("${time-zone-interceptor.header-name:X-TimeZone}")
     private String timeZoneHeaderName;
 
-    //Tests won't work without explicitly setting the header name
+    /**
+     * Sets the time zone for the current thread based on the value of the "timeZoneHeaderName" header in the HttpServletRequest object.
+     *
+     * @param request The HttpServletRequest object representing the current request.
+     * @param response The HttpServletResponse object representing the current response.
+     * @param handler The Object representing the handler that is being executed for the current request.
+     * @return true if the request should proceed with further processing, false if it should be terminated.
+     * @throws Exception if an error occurs while processing the request.
+     */ //Tests won't work without explicitly setting the header name
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("TimeZoneInterceptor:: Inside preHandle");
@@ -31,6 +42,16 @@ public class TimeZoneInterceptor  implements HandlerInterceptor {
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
+    /**
+     * Performs cleanup after the request has been completed.
+     * Removes the time zone from the current thread.
+     *
+     * @param request The HttpServletRequest object representing the completed request.
+     * @param response The HttpServletResponse object representing the completed response.
+     * @param handler The Object representing the handler that was executed for the request.
+     * @param ex The Exception that occurred during the request processing, or null if no exception occurred.
+     * @throws Exception if an error occurs while performing cleanup.
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         logger.info("TimeZoneInterceptor:: Inside afterCompletion");
@@ -38,6 +59,11 @@ public class TimeZoneInterceptor  implements HandlerInterceptor {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
     
+    /**
+     * Returns the time zone associated with the current thread.
+     *
+     * @return the time zone associated with the current thread
+     */
     public static TimeZone getTimeZone() {
         return TIME_ZONE_THREAD_LOCAL.get();
     }
